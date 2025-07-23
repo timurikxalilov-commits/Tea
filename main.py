e25c6d8bf5159c38790f611d8bdc2a298d96a83f
timurikxalilov-commits <timurik.xalilov@gmail.com>
2025-07-24 03:48:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import random
 
 # 🔧 Конфигурация
 MASTER_CHAT_ID = 5225197085
-TOKEN = "7436013012:AAFmxpR03fQC_VOj_pWKhyfaK43FohaPNoE"
+TOKEN = "7436013012:AAFyD5YEYS7toek2quD8P7N71lmiYz_RwtY"
 
 # 📂 Состояния
 NAME, DATE, PLACE, COMMENTS, PHONE, NOTE, REMIND = range(7)
